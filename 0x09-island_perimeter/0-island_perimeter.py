e13bdcd40aa1bd7d1293c0991288b0a542a6e53b
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-26 23:28:56 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""Island perimeter module
+"""Island perimeter module.
 """
 
 
 def island_perimeter(grid):
+    """perimeter of an island with no lakes.
+    """
     perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-
-    for row_index in range(rows):
-        for col_index in range(cols):
-            if grid[row_index][col_index] == 1:
-                perimeter += 4
-                if row_index > 0 and grid[row_index - 1][col_index] == 1:
-                    perimeter -= 2
-                if col_index > 0 and grid[row_index][col_index - 1] == 1:
-                    perimeter -= 2
-
+    if type(grid) != list:
+        return 0
+    z = len(grid)
+    for a, r in enumerate(grid):
+        y = len(r)
+        for b, unit in enumerate(r):
+            if unit == 0:
+                continue
+            linits = (
+                a == 0 or (len(grid[a - 1]) > b and grid[a - 1][b] == 0),
+                b == y - 1 or (y > b + 1 and r[b + 1] == 0),
+                a == z - 1 or (len(grid[a + 1]) > b and grid[a + 1][b] == 0),
+                b == 0 or r[b - 1] == 0,
+            )
+            perimeter += sum(linits)
     return perimeter
