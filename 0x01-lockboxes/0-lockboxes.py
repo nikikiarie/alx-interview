dba0b95ac04d9427766adc01a7b3267bb2756d86
nikikiarie <nix.kiariebiz@gmail.com>
2024-11-07 22:05:51 +0300

    lockbox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Checks if a collection of boxes can be unlocked with keys found inside them."""
+
+def canUnlockAll(containers):
+    """Determines if all containers can be opened"""
+    index = 0
+    accessible = {}
+
+    for container in containers:
+        if len(container) == 0 or index == 0:
+            accessible[index] = "always_open"
+        for code in container:
+            if code < len(containers) and code != index:
+                accessible[code] = code
+        if len(accessible) == len(containers):
+            return True
+        index += 1
+    return False
